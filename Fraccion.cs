871525cc342c6d09012a496a781dc6c8e8233d19
JazzLilo <63597140+JazzLilo@users.noreply.github.com>
2020-04-13 15:21:14 -0400

    Add files via upload

---
+++ Fraccion.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pinche_Josu
+{
+    class Fraccion
+    {
+		private int num;
+		private int den;
+
+		public int Num
+		{
+			get { return den; }
+			set { num = value; }
+		}
+
+
+		public Fraccion(int num, int den)
+		{
+			this.num = num;
+			this.den = den;
+		}
+		public void mostrar()
+		{
+			Console.WriteLine("\nObjeto Fraccion");
+			Console.WriteLine(num + "/" + den);
+		}
+		public void lee()
+		{
+			num = Int32.Parse(Console.ReadLine());
+			den = Int32.Parse(Console.ReadLine());
+
+		}
+		public static Fraccion operator +(Fraccion p, Fraccion q)
+		{
+			int nume = (p.num * q.den) + (p.den * q.num);
+			int deno = (p.den * q.den);
+			return (new Fraccion(nume, deno));
+		}
+		public static Fraccion operator -(Fraccion p, Fraccion q)
+		{
+			int nume = (p.num * q.den) - (p.den * q.num);
+			int deno = p.den * q.den;
+			return (new Fraccion(nume, deno));
+		}
+		public static Fraccion operator *(Fraccion p, Fraccion q)
+		{
+			int nume = p.num * q.num;
+			int deno = p.den * q.den;
+			return (new Fraccion(nume, deno));
+		}
+		public static Fraccion operator /(Fraccion p, Fraccion q)
+		{
+			int nume = p.num * q.den;
+			int deno = p.den * q.num;
+			return (new Fraccion(nume, deno));
+		}
+
+	}
+}
---
+++ Mani.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pinche_Josu
+{
+    class Mani
+    {
+        static void Main(string[] args)
+        {
+			int sw;
+			Console.WriteLine("Elija una opcion\n" +
+			"\n1. Crear dos objetos fraccion" +
+			"\n2. Suma" +
+			"\n3. Resta" +
+			"\n4. Producto" +
+			"\n5. Division" +
+			"\n6. Simplificar fraccion\n" +
+			"\n7. Me la Pelas Josu :v");
+			Fraccion f3;
+			Fraccion f1 = new Fraccion(2, 4);
+			Fraccion f2 = new Fraccion(5, 8);
+			do
+			{
+				sw = Int32.Parse(Console.ReadLine());
+				switch (sw)
+				{
+					case 1:
+						Console.WriteLine("Fraccion 1");
+						f1.lee();
+						f1.mostrar();
+						Console.WriteLine("Fraccion 2 ");
+						f2.lee();
+						f2.mostrar();
+						break;
+					case 2:
+						Console.WriteLine("opcion suma");
+
+						f3 = f1 + f2;
+						f3.mostrar();
+						Console.WriteLine("Me la pelas Josu");
+						break;
+					case 3:
+						Console.WriteLine("opcion resta");
+						f3 = f1 - f2;
+						f3.mostrar();
+						Console.WriteLine("Me la pelas Josu");
+						break;
+					case 4:
+						Console.WriteLine("opcion producto");
+						f3 = f1 * f2;
+						f3.mostrar();
+						Console.WriteLine("Me la pelas Josu");
+						break;
+					case 5:
+						Console.WriteLine("opcion division");
+						f3 = f1 / f2;
+						f3.mostrar();
+						Console.WriteLine("Me la pelas Josu");
+						break;
+					case 6:
+						Console.WriteLine("fraccion simplificada....");
+						Console.WriteLine("Me la pelas Josu");
+						break;
+					default:
+						sw = 7;//tienes solo 6 casos por eso 7 :v 
+						break;
+				}
+			} while (sw != 7);
+			Console.ReadKey();
+		}
+    }
+}
